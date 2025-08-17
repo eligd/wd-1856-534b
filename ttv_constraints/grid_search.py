fix bug in chi2 computation

ttv_constraints/grid_search.py
@@ -43,48 +43,37 @@ def linear_model(x, m, b):
 
 def get_chi2(obs, exp, uncertainty):
     epochs_obs = get_epoch(obs)
-    epochs_exp = get_epoch(exp)
 
-    popt_obs, _ = curve_fit(linear_model, epochs_obs, obs, sigma=uncertainty)
-    popt_exp, _ = curve_fit(linear_model, epochs_exp, exp, sigma=uncertainty)
+    popt, _ = curve_fit(linear_model, epochs_obs, obs, sigma=uncertainty, absolute_sigma=True)
+    m, b = popt
+    const_P_model = m * epochs_obs + b
 
-    m_obs, b_obs = popt_obs
-    m_exp, b_exp = popt_exp
+    ttv_const_P = obs - const_P_model
+    ttv_model = obs - exp
 
-    model_obs = m_obs * epochs_obs + b_obs
-    model_exp = m_exp * epochs_exp + b_exp
-
-    ttv_obs = obs - model_obs
-    ttv_exp = exp - model_exp
-
-    chi2 = np.sum((ttv_obs - ttv_exp)**2 / uncertainty**2)
+    chi2 = np.sum((ttv_const_P - ttv_model)**2 / uncertainty**2)
     return chi2
 
 def loop_body(i):
     mass_idx = i // (p2_period_points * p2_mean_anomaly_points)
     period_idx = (i % (p2_period_points * p2_mean_anomaly_points)) // p2_mean_anomaly_points
     mean_anomaly_idx = i % p2_mean_anomaly_points
-
+        
     theta = [p2_masses[mass_idx], p2_periods[period_idx], p2_eccentricity, p2_inclination, 
             p2_longnode, p2_argument, p2_mean_anomalies[mean_anomaly_idx]]
     t_pred = get_transit_time_predictions(theta)
     epochs_pred = get_epoch(t_pred)
 
-    t_const_P_pred = t_pred[0] + p1_period * (epochs_pred - epochs_pred[0])
-    mm = t_pred - t_const_P_pred
-    exp_ttv = []
     tt = []
     for epoch in epochs_obs:
         idx = np.where(epochs_pred == epoch)[0]
         if idx.size != 0:
-            exp_ttv.append(mm[idx[0]])
             tt.append(t_pred[idx[0]])
-    exp_ttv = np.array(exp_ttv)
     tt = np.array(tt)
     R = romer_delay(p1_period, p2_periods[period_idx], p1_mass, p2_masses[mass_idx], stellar_mass, p1_inclination, ref_transit, tt)
-    exp_ttv += R
-
-    chi2 = get_chi2(obs_ttv, exp_ttv, t_obs_err)
+    tt += R
+    
+    chi2 = get_chi2(t_obs, tt, t_obs_err)
     return chi2
 
 
@@ -161,8 +150,6 @@ obs_ttv = t_obs - t_const_P
 
 
 if __name__ == '__main__':
-    print("Constant period model chi2: ", get_chi2(obs_ttv, np.zeros_like(obs_ttv), t_obs_err))
-
     grid_params = np.meshgrid(p2_masses, p2_periods, p2_mean_anomalies, indexing='ij')
     chi2_arr = []
     bar_format = '{l_bar}{bar:20}{r_bar}{bar:-10b}'

ttv_constraints/utils_plot.py
@@ -1,58 +1,81 @@
 import numpy as np
+from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 plt.rcParams['font.family'] = 'serif'
 
-def plot_transit_times(t_obs, epochs_obs, t_pred, epochs_pred, period, savepath):
-    fig, ax = plt.subplots(figsize=(8, 8))
-    fontsize = 20
-    leg_size = 14
+def plot_transit_times(t_obs, epochs_obs, t_pred, epochs_pred, uncertainty, savepath):
+    fig, axs = plt.subplots(2, 1, figsize=(12, 10))
+    plt.subplots_adjust(hspace=0.2)
+    fontsize = 14
+    leg_size = 13
 
-    ax.minorticks_on()
-    ax.tick_params(axis='both', labelsize=12)
-    ax.tick_params(direction='in', which='minor', length=5, bottom=True, top=True, left=True, right=True)
-    ax.tick_params(direction='in', which='major', length=10, bottom=True, top=True, left=True, right=True)
+    for ax in axs:
+        ax.minorticks_on()
+        ax.tick_params(axis='both', labelsize=12)
+        ax.tick_params(direction='in', which='minor', length=5, bottom=True, top=True, left=True, right=True)
+        ax.tick_params(direction='in', which='major', length=10, bottom=True, top=True, left=True, right=True)
 
-    t_const_P_obs = t_obs[0] + period * (epochs_obs - epochs_obs[0])
-    t_const_P_pred = t_pred[0] + period * (epochs_pred - epochs_pred[0])
+    def linear_model(x, m, b):
+        return m * x + b
 
-    ax.plot(epochs_obs, (t_obs-t_const_P_obs)*24*60, color='tab:blue', linestyle='', marker='.', markersize=10, label='Observed')
-    ax.plot(epochs_pred, (t_pred-t_const_P_pred)*24*60, color='tab:red', linestyle='-', label='Predicted')
+    popt_obs, _ = curve_fit(linear_model, epochs_obs, t_obs, sigma=uncertainty, absolute_sigma=True)
 
-    xmin, xmax = ax.get_xlim()
-    ax.plot([xmin, xmax], [0, 0], color='black', linewidth=2, label='Constant Period')
-    ax.set_ylim(-0.5, 0.5)
+    m_obs, b_obs = popt_obs
 
-    ax.set_xlabel('Epoch Number', fontsize=fontsize)
-    ax.set_ylabel('Transit Timing Variation [min]', fontsize=fontsize)
+    t_const_P_obs = m_obs * epochs_obs + b_obs
 
-    ax.legend(loc='upper right', fontsize=leg_size, framealpha=1)
+    tt = []
+    for epoch in epochs_obs:
+        idx = np.where(epochs_pred == epoch)[0]
+        if idx.size != 0:
+            tt.append(t_pred[idx[0]])
+    tt = np.array(tt)
+
+    axs[0].plot(epochs_obs, (t_obs-t_const_P_obs)*24*60, color='tab:blue', linestyle='', marker='.', markersize=10, label='Observed')
+    axs[1].plot(epochs_obs, (t_obs-tt)*24*60, color='tab:blue', linestyle='', marker='.', markersize=10, label='Observed')
+
+    print(t_obs-tt)
+
+    labels = ['Constant Period', 'Companion Model']
+    for ax, label in zip(axs, labels):
+        xmin, xmax = ax.get_xlim()
+        ax.plot([xmin, xmax], [0, 0], color='black', linewidth=2, label=label)
+        ax.set_ylim(-0.5, 0.5)
+
+        ax.set_xlabel('Epoch Number', fontsize=fontsize)
+        ax.set_ylabel('Transit Timing Variation [min]', fontsize=fontsize)
+
+        ax.legend(loc='upper right', fontsize=leg_size, framealpha=1)
 
     plt.savefig(savepath, format='pdf', bbox_inches='tight');
 
-def plot_likelihood_ratio(chi2_arr, savepath):
-    fig, ax = plt.subplots(figsize=(10, 8))
+def plot_likelihood_ratio(chi2_arr, savepath, title=None):
+    fig, ax = plt.subplots(figsize=(15, 8))
     fontsize = 20
+    title_size = 25
 
-    const_P_chi2 = 91.73502023749768
+    const_P_chi2 = 91.7350202373991
     likelihood_ratio_log_base_e = - (chi2_arr - const_P_chi2) / 2
     likelihood_ratio_log_base_10 = likelihood_ratio_log_base_e * np.log10(np.e)
     likelihood_ratio = np.max(likelihood_ratio_log_base_10, axis=-1) # collapse along mean anomaly axis
+    print("Max likelihood ratio: ", np.max(10**likelihood_ratio))
+    print("Max likelihood ratio idx: ", np.argmin(chi2_arr))
+    print("Min chi2: ", np.min(chi2_arr))
 
-    im = ax.imshow(likelihood_ratio.T, extent=[0.000003*1047.57, 0.0124*1047.57, 2000, 100], vmin=-3, vmax=3)
+    im = ax.imshow(likelihood_ratio.T, extent=[0.000003*1047.57, 0.0124*1047.57, 1500, 50], vmin=-4, vmax=4)
     ax.set_aspect('auto')
     ax.set_xlabel('Mass [Jupiter masses]', fontsize=fontsize)
     ax.set_ylabel('Period [days]', fontsize=fontsize)
 
     cbar = plt.colorbar(im, ax=ax)
     cbar.set_label('Log Base 10 Likelihood Ratio', rotation=90, labelpad=15, fontsize=18)
+
+    if title is not None:
+        plt.title(title, fontsize=title_size, y=1.02)
     
     plt.savefig(savepath, format='pdf', bbox_inches='tight')
 
 if __name__ == '__main__':
-    chi2_arr = np.load('arrays/chi2_no_romer_correction.npy')
-    savepath = 'plots/likelihood_ratio_no_romer_correction.pdf'
-    plot_likelihood_ratio(chi2_arr, savepath)
-
-    chi2_arr = np.load('arrays/chi2_romer_correction.npy')
-    savepath = 'plots/likelihood_ratio_romer_correction.pdf'
+    chi2_arr = np.load('arrays/chi2_new.npy')
+    savepath = 'plots/likelihood_ratios_new.pdf'
     plot_likelihood_ratio(chi2_arr, savepath)